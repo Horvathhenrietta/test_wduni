875fc719e78c8f2f0cb954c7bf39a2152e47f413
Henrietta <h_henrietta@outlook.com>
2023-01-27 21:42:16 +0100

    Added tests into test_todolist.py

--- test_button_clicks.py
+++ test_button_clicks.py
@@ -6,6 +6,13 @@ from selenium.webdriver.common.by import By
 class Login(unittest.TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Chrome()
-        self.driver.get("http://www.webdriveruniversity.com/Login-Portal/index.html")
+        self.driver.get("http://www.webdriveruniversity.com/Click-Buttons/index.html")
 
-    def
+    def test_webelement_click(self):
+        self.driver.find_element(By.ID, "button1").click()
+        modal = self.driver.find_element(By.ID, "myModalClick")
+        self.assertTrue(modal)
+
+
+    def tearDown(self) -> None:
+        self.driver.quit()
---
+++ test_todolist.py
@@ -0,0 +1,37 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class ToDoList(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://www.webdriveruniversity.com/To-Do-List/index.html")
+
+    def test_add_new_to_do(self):
+        # given
+        to_do_text = "test"
+        self.input = self.driver.find_element(By.XPATH, "//input")
+        # when
+        self.input.send_keys(to_do_text + Keys.RETURN)
+        # then
+        self.assertTrue(to_do_text in self.driver.find_element(By.XPATH, "//li[4]").text)
+
+    def test_delete_to_do(self):
+        # given
+        a = ActionChains(self.driver)
+        container_el = self.driver.find_element(By.XPATH, "//ul")
+        to_do_el = self.driver.find_elements(By.XPATH, "//li[1]")
+        trash_el = self.driver.find_element(By.XPATH, "//li[1]/span/i")
+        # when
+        a.move_to_element(to_do_el[0]).perform()
+        trash_el.click()
+        to_do_el = self.driver.find_elements(By.XPATH, "//li[1]")
+
+        # then
+        self.assertTrue(to_do_el[0].text != " Go to potion class")
+
+    def tearDown(self):
+        self.driver.quit()
