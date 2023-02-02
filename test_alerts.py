75dc00ef773afac22cd141ad093d545bdf4d1101
Henrietta <h_henrietta@outlook.com>
2023-02-02 13:16:26 +0100

    Added tests to test_alerts.py

--- test_alerts.py
+++ test_alerts.py
@@ -21,17 +21,46 @@ class Alerts(unittest.TestCase):
         alert = Alert(self.driver)
         # then
         self.assertEqual(alert.text, "I am an alert box!")
+        alert.accept()
 
     def test_modal_popup(self):
         # given
         wait = WebDriverWait(self.driver, 10)
         btn = self.driver.find_element(By.ID, "button2")
         modal = self.driver.find_element(By.ID, "myModal")
+        close_btn = self.driver.find_element(By.XPATH, "///button[@innertext='Close']")
         # when
         btn.click()
         wait.until(EC.visibility_of_element_located((By.ID, "myModal")))
         # then
         self.assertEqual(modal.value_of_css_property("display"), "block")
+        close_btn.click()
+
+    def test_js_confirm_box_accept(self):
+        # given
+        wait = WebDriverWait(self.driver, 10)
+        btn = self.driver.find_element(By.ID, "button4")
+        confirm_alert_p = self.driver.find_element(By.ID, "confirm-alert-text")
+        # when
+        btn.click()
+        wait.until(EC.alert_is_present())
+        alert = Alert(self.driver)
+        alert.accept()
+        # then
+        self.assertEqual(confirm_alert_p.text, "You pressed OK!")
+
+    def test_js_confirm_box_cancel(self):
+        # given
+        wait = WebDriverWait(self.driver, 10)
+        btn = self.driver.find_element(By.ID, "button4")
+        confirm_alert_p = self.driver.find_element(By.ID, "confirm-alert-text")
+        # when
+        btn.click()
+        wait.until(EC.alert_is_present())
+        alert = Alert(self.driver)
+        alert.dismiss()
+        # then
+        self.assertEqual(confirm_alert_p.text, "You pressed Cancel!")
 
     def tearDown(self) -> None:
         self.driver.quit()
